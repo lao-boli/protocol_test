479a6d405de4e633086fb856143d912acea2a968
laoboli <1293528695@qq.com>
2023-03-05 10:30:36 +0800

    feat: 添加mqtt服务端和客户端(未完成).

---
+++ src/main/java/org/hqu/lly/protocol/mqtt/client/handler/MQTTClientConnectHandler.java
@@ -0,0 +1,42 @@
+package org.hqu.lly.protocol.mqtt.client.handler;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+import org.hqu.lly.service.impl.ClientService;
+
+/**
+ * <p>
+ * TCP客户端管理Channel连接的处理器
+ * <p>
+ *
+ * @author hqully
+ * @version 1.0
+ * @date 2022/9/17 08:44
+ */
+@Slf4j
+public class MQTTClientConnectHandler extends ChannelInboundHandlerAdapter {
+
+    private ClientService clientService;
+
+    public MQTTClientConnectHandler(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        log.info("client: {} connect to server: {}", channel.localAddress(), channel.remoteAddress());
+        clientService.updateMsgList("连接成功, 服务端地址为: " + channel.remoteAddress().toString());
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        log.info("client: {} disconnect form server: {}", channel.localAddress(), channel.remoteAddress());
+        clientService.updateMsgList("与服务端断开连接, 服务端地址: " + channel.remoteAddress().toString());
+        clientService.onClose();
+    }
+
+}
---
+++ src/main/java/org/hqu/lly/protocol/mqtt/server/group/MQTTChannelGroup.java
@@ -0,0 +1,25 @@
+package org.hqu.lly.protocol.mqtt.server.group;
+
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * <p>
+ * channel管理组
+ * <p>
+ *
+ * @author hqully
+ * @version 1.0
+ * @date 2022/8/9 15:57
+ */
+public class MQTTChannelGroup {
+
+    public static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    public static Map<String, Channel> clientChannelGroup = new ConcurrentHashMap<>();
+    public static Map<String, Channel> serverChannelGroup = new ConcurrentHashMap<>();
+}
