9c28f60ace1a9f76c3eab4bf39c26c6f0e1da680
laoboli <1293528695@qq.com>
2023-03-11 21:17:06 +0800

    feat: 新增mqtt话题树.

---
+++ src/main/java/org/hqu/lly/protocol/mqtt/router/NodeType.java
@@ -0,0 +1,29 @@
+package org.hqu.lly.protocol.mqtt.router;
+
+/**
+ * <p>
+ * 话题路由节点类型枚举
+ * <p>
+ *
+ * @author hqully
+ * @version 1.0
+ * @date 2023/3/5 11:00
+ */
+enum NodeType {
+    /**
+     * 根节点
+     */
+    ROOT,
+    /**
+     * 不含channel的静态路径节点
+     */
+    STATIC,
+    /**
+     * 单层通配符: "+"
+     */
+    SINGLE,
+    /**
+     * 多层通配符: "#"
+     */
+    MULTI;
+}
