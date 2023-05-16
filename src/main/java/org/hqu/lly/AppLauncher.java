31963318680493722e03c98353f01aa5abfc7bdd
laoboli <1293528695@qq.com>
2023-05-16 08:09:15 +0800

    fix: 修复中文乱码的问题.

--- src/main/java/org/hqu/lly/AppLauncher.java
+++ src/main/java/org/hqu/lly/AppLauncher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
  
 public class AppLauncher {
     public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
         Application.launch(App.class, args);
     }
 }
