78156b3cff0f4414481ef038f0de47d497003982
laoboli <1293528695@qq.com>
2023-08-02 20:23:37 +0800

    doc: add js lib doc.

--- src/main/resources/js/random-util.js
+++ src/main/resources/js/random-util.js
@@ -1,3 +1,10 @@
+/**
+ * generate a random integer number within the specified range
+ * @param low number low bound
+ * @param high number high bound
+ * @param radix number radix, default is 10
+ * @returns {number} generated number
+ */
 function randInt(low, high, radix) {
     radix = radix || 10;
     var lowDeci = parseInt(low, radix)
@@ -6,12 +13,23 @@ function randInt(low, high, radix) {
     return res;
 }
 
+/**
+ * generate a double number within the specified range
+ * @param low low bound
+ * @param high high bound
+ * @returns {*} generated double number
+ */
 function randDouble(low, high) {
     var range = high - low;
     var rand = Math.random();
     return low + range * rand;
 }
 
+/**
+ * get a random element from the input array
+ * @param arr input array
+ * @returns {*} a random element in input array
+ */
 function randArr(arr) {
     var i = randInt(0,arr.length - 1);
     return arr[i];
