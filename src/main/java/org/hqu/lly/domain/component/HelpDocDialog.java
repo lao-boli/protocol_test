e23f3df96a7f777194ef5914be049f489ea98bfd
laoboli <1293528695@qq.com>
2023-08-11 10:40:54 +0800

    refactor: complete anim.

--- src/main/java/org/hqu/lly/domain/component/HelpDocDialog.java
+++ src/main/java/org/hqu/lly/domain/component/HelpDocDialog.java
@@ -1,5 +1,10 @@
 package org.hqu.lly.domain.component;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -8,6 +13,7 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.hqu.lly.constant.ResLoc;
 import org.hqu.lly.icon.LeftArrowIcon;
 import org.hqu.lly.icon.RightArrowIcon;
@@ -24,6 +30,11 @@ import org.hqu.lly.icon.RightArrowIcon;
 public class HelpDocDialog extends MyDialog<HelpDocDialog.ContentPane> {
 
     private final ContentPane contentPane;
+
+    enum Direction {
+        PREV,
+        NEXT
+    }
 
     @Override
     public void close() {
@@ -43,6 +54,8 @@ public class HelpDocDialog extends MyDialog<HelpDocDialog.ContentPane> {
         // css
         pane.getStylesheets().add(ResLoc.HELP_DOC_DIALOG_CSS.toExternalForm());
         contentPane.addText("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        contentPane.addText("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        contentPane.addText("ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc");
 
     }
 
@@ -55,9 +68,11 @@ public class HelpDocDialog extends MyDialog<HelpDocDialog.ContentPane> {
 
         // region base UI controls
         HBox header = new HBox();
-        HBox container = new HBox();
+        StackPane container = new StackPane();
         HBox btnGroup = new HBox();
         //endregion
+
+        private int currentIndex = 2;
 
         public ContentPane() {
             init();
@@ -70,7 +85,7 @@ public class HelpDocDialog extends MyDialog<HelpDocDialog.ContentPane> {
             setupBtnGroup();
 
             this.getStyleClass().add("content-pane");
-            this.getChildren().addAll(header,container, btnGroup);
+            this.getChildren().addAll(header, container, btnGroup);
         }
 
 
@@ -79,11 +94,14 @@ public class HelpDocDialog extends MyDialog<HelpDocDialog.ContentPane> {
 
             Button nextBtn = new Button();
             nextBtn.setGraphic(new RightArrowIcon());
+            nextBtn.setOnMouseClicked(event -> switchToNextPane());
             Button prevBtn = new Button();
             prevBtn.setGraphic(new LeftArrowIcon());
+            prevBtn.setOnMouseClicked(event -> switchToPreviousPane());
 
             header.getChildren().addAll(prevBtn, nextBtn);
         }
+
         private void setupBtnGroup() {
             btnGroup.getStyleClass().add("btn-group");
             Button closeBtn = new Button("关闭");
@@ -92,20 +110,81 @@ public class HelpDocDialog extends MyDialog<HelpDocDialog.ContentPane> {
 
         void setupContent() {
             container.getStyleClass().add("container");
-            VBox.setVgrow(container,Priority.ALWAYS);
+            VBox.setVgrow(container, Priority.ALWAYS);
         }
 
         void addText(String text) {
-            Node c = createContent(text);
-            container.getChildren().add(c);
+            container.getChildren().add(new DocNode(text));
         }
-        Node createContent(String text) {
-            TextArea textArea = new TextArea(text);
+
+        ObservableList<Node> getDocNodes() {
+            return container.getChildren();
+        }
+
+
+        private void switchToPreviousPane() {
+            int previousIndex = currentIndex == 0 ? getDocNodes().size() - 1 : currentIndex - 1;
+            switchPane(previousIndex, Direction.PREV);
+        }
+
+        private void switchToNextPane() {
+            int nextIndex = currentIndex == getDocNodes().size() - 1 ? 0 : currentIndex + 1;
+            switchPane(nextIndex, Direction.NEXT);
+        }
+
+        private void switchPane(int newIndex, Direction direction) {
+            Node curNode = getDocNodes().get(currentIndex);
+            Node nextNode = getDocNodes().get(newIndex);
+
+            curNode.setVisible(true);
+            nextNode.setVisible(true);
+            getDocNodes().forEach(n -> {
+                if (!n.equals(curNode) && !n.equals(nextNode)){
+                    n.setVisible(false);
+                }
+            });
+
+            double targetX = getWidth();
+            if (Direction.PREV.equals(direction)){
+                targetX = -targetX;
+            }
+
+            nextNode.setTranslateX(targetX);
+
+            Timeline timeline = new Timeline(
+                    new KeyFrame(Duration.ZERO, new KeyValue(curNode.translateXProperty(), 0)),
+                    new KeyFrame(Duration.ZERO, new KeyValue(nextNode.translateXProperty(), targetX)),
+                    new KeyFrame(Duration.seconds(0.5), new KeyValue(curNode.translateXProperty(), -targetX, Interpolator.EASE_BOTH)),
+                    new KeyFrame(Duration.seconds(0.5), new KeyValue(nextNode.translateXProperty(), 0, Interpolator.EASE_BOTH))
+            );
+
+            timeline.setAutoReverse(false);
+            timeline.setCycleCount(1);
+
+            timeline.setOnFinished(event -> {
+                currentIndex = newIndex;
+                curNode.setVisible(false);
+                nextNode.setVisible(true);
+            });
+
+            timeline.play();
+        }
+
+        void initAnim() {
+
+        }
+
+    }
+
+    class DocNode extends StackPane {
+
+        private final TextArea textArea;
+
+        public DocNode(String text) {
+            textArea = new TextArea(text);
             textArea.setEditable(false);
             textArea.setWrapText(true);
-            StackPane stackPane = new StackPane(textArea);
-            HBox.setHgrow(stackPane,Priority.ALWAYS);
-            return stackPane;
+            getChildren().add(textArea);
         }
 
     }
