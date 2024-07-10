1d743679b766fc46ccc379e35c7170ac9dd64fe2
laoboli <1293528695@qq.com>
2024-07-10 22:04:37 +0800

    test: add test.

---
+++ src/test/java/org/hqu/lly/SendSettingPaneLauncher.java
@@ -0,0 +1,11 @@
+package org.hqu.lly;
+
+import javafx.application.Application;
+import org.hqu.lly.domain.component.SendSettingPaneTest;
+
+public class SendSettingPaneLauncher {
+
+    public static void main(String[] args) {
+        Application.launch(SendSettingPaneTest.class, args);
+    }
+}
